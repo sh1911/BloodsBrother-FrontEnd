da361cd3f7e37b13f2c69e8efc15c15b39a36c93
sha911 <shaikmohammed@shaiknoor.root>
2020-11-24 10:18:29 +0530

    removed  commit

--- forexaggregatorfrontend-app/src/skeletons/SkeletonElement.js
+++
@@ -1,9 +0,0 @@
-import React  from 'react';
-import './Skeleton.css';
-const SkeletonElement =({type})=>{
-    const classes=`skeleton ${type}`;
-    return(
-        <div className={classes}></div>
-    )
-}
-export default SkeletonElement;
